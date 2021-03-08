efb95f795b8346ad6861ccb25d5e0567db45c129
WazeAzure <bryan.rich0604@gmail.com>
2021-03-08 10:48:46 +0800

    add all files

--- src/components/Contributor/ContributorCard.js
+++ src/components/Contributor/ContributorCard.js
@@ -4,8 +4,8 @@ const ContributorCard = () => {
     <>
       <h1 style={{ textAlign: "center", margin: "5px" }}>Contributor</h1>
       <div className={classes.FlipCard}>
-        <div className={classes.FlipCardInner}>
-          <div className={classes.FlipCardFront}>
+        <div className={classes.FlipCardInner }>
+          <div className={classes.FlipCardFront }>
             <img
               className={classes.Image}
               src={`https://robohash.org/2?set=set3`}
@@ -13,7 +13,7 @@ const ContributorCard = () => {
             />
             <h1>John Doe</h1>
           </div>
-          <div className={classes.FlipCardBack}>
+          <div className={classes.FlipCardBack }>
             <p>Architect & Engineer</p>
             <p>We love that guy</p>
           </div>
